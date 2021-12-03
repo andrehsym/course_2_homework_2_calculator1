90fd04a31d2f059bd94faa59344c922821f03753
andrehsym <kochetkovandre1993@gmail.com>
2021-12-03 23:57:02 +0300

    Готовое ДЗ

--- src/main/java/pro/sky/java/homeworks/course2/calculator1/controller1/Calculator1Controller.java
+++ src/main/java/pro/sky/java/homeworks/course2/calculator1/controller1/Calculator1Controller.java
@@ -1,13 +1,42 @@
 package pro.sky.java.homeworks.course2.calculator1.controller1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/calculator")
 public class Calculator1Controller {
 
+    private final Calculator1Service calculator1Service;
+
+    public Calculator1Controller(Calculator1Service calculator1Service) {
+        this.calculator1Service = calculator1Service;
+    }
+
     @GetMapping
-    public String hello() {
-        return "Hello";
+    public String welcomeCalculator() {
+        return calculator1Service.hello();
+    }
+
+    @GetMapping("/plus")
+    public String plus1(@RequestParam (required = true) int num1, @RequestParam (required = true) int num2) {
+        return calculator1Service.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam (required = true) int num1, @RequestParam (required = true) int num2) {
+        return calculator1Service.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam (required = true) int num1, @RequestParam (required = true) int num2) {
+        return calculator1Service.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam (required = true) int num1, @RequestParam (required = true) int num2) {
+        return calculator1Service.divide(num1, num2);
     }
 }
---
+++ src/main/java/pro/sky/java/homeworks/course2/calculator1/controller1/Calculator1Service.java
@@ -0,0 +1,23 @@
+package pro.sky.java.homeworks.course2.calculator1.controller1;
+
+public interface Calculator1Service {
+
+    int plusInt(int num1, int num2);
+
+    int minusInt(int num1, int num2);
+
+    int multiplyInt(int num1, int num2);
+
+    int divideInt(int num1, int num2);
+
+    String hello();
+
+    String plus(int num1, int num2);
+
+    String minus(int num1, int num2);
+
+    String multiply(int num1, int num2);
+
+    String divide(int num1, int num2);
+
+}
